4597d2e0f102b3bd7ebfa48cecaf661eb16e90f5
Madison <madisonthompsonld@gmail.com>
2021-01-18 15:32:12 -0600

    Created RollingDice file

---
+++ Project1/src/RollingDice/RollingDice.java
@@ -0,0 +1,10 @@
+package RollingDice;
+
+public class RollingDice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("You rolled a: 6");
+	}
+
+}
